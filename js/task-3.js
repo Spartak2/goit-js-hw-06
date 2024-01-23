1688f3f9b1b645e817b2290a0db2ae07cf895516
Spartak2 <orkotop030@gmail.com>
2024-01-24 01:22:48 +0200

    js.2

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,16 @@
 "use strict";
 
-class StringBuilder {
+  class StringBuilder {
     constructor(initialValue = '') {
       this.value = initialValue;
     }
-    getValue(str) {
+
+    getValue() {
       return this.value;
     }
 
     padEnd(str) {
-      this.value =+ str;
+      this.value += str;
     }
 
     padStart(str) {
@@ -19,13 +20,14 @@ class StringBuilder {
     padBoth(str) {
       this.value = str + this.value + str;
     }
-}
+  }
 
-
-// Використання класу зазначене в завданні:
+  // Використання класу зазначене в завданні:
   const builder = new StringBuilder(".");
   console.log(builder.getValue()); // "."
   builder.padStart("^");
   console.log(builder.getValue()); // "^."
   builder.padEnd("^");
   console.log(builder.getValue()); // "^.^"
+  builder.padBoth("=");
+  console.log(builder.getValue()); // "=^.^="
